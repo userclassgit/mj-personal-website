04a7ec41c7d451aea1ff5838495d91d9811eb2a5
Myeongjeon Lee <userclassdev@outlook.com>
2024-03-08 21:56:05 -0600

    add footer. made some changes to contact section

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,12 @@ import * as utils from './utils.js';
 
 'use strict';
 const emailButton = utils.select("button");
+const footerEmailButton = utils.select(".fa-envelope");
 
 const emailCallBack = () => {
     location.href = "mailto:userclassdev@outlook.com?subject=";
 }
 utils.listen('click', emailButton, emailCallBack);
+utils.listen('click', footerEmailButton, emailCallBack);
 
 
